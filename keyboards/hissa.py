6988477b643e63854af217ebca50dac8cb11cd43
Maxvin11 <asadbekahmedov16@gmail.com>
2026-01-08 11:37:11 +0500

    fixed: hissa

--- keyboards/hissa.py
+++ keyboards/hissa.py
@@ -3,6 +3,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 hissa = InlineKeyboardMarkup(inline_keyboard=[
     [
-        InlineKeyboardButton(text="Hissa qo'shish 💰", url = "https://t.me/ataullayevvv")
+        InlineKeyboardButton(text="Hissa qo'shish 💰", url = "https://t.me/timurbek_ustozai")
     ]
 ])
